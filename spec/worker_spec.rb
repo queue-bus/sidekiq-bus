25d33f8f26b6fef585bc0c856e8ab248e87b6946
kbacha <chewbacha@gmail.com>
2019-05-16 09:52:48 -0700

    Replaces should syntax with expect

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -4,28 +4,28 @@ module QueueBus
   describe Worker do
     it "should proxy to given class" do
       hash = {"bus_class_proxy" => "QueueBus::Driver", "ok" => true}
-      QueueBus::Driver.should_receive(:perform).with(hash)
+      expect(QueueBus::Driver).to receive(:perform).with(hash)
       QueueBus::Worker.perform(JSON.generate(hash))
     end
 
     it "should use instance" do
       hash = {"bus_class_proxy" => "QueueBus::Rider", "ok" => true}
-      QueueBus::Rider.should_receive(:perform).with(hash)
+      expect(QueueBus::Rider).to receive(:perform).with(hash)
       QueueBus::Worker.new.perform(JSON.generate(hash))
     end
 
     it "should not freak out if class not there anymore" do
       hash = {"bus_class_proxy" => "QueueBus::BadClass", "ok" => true}
 
-      QueueBus::Worker.perform(JSON.generate(hash)).should == nil
+      expect(QueueBus::Worker.perform(JSON.generate(hash))).to eq(nil)
     end
 
     it "should raise error if proxy raises error" do
       hash = {"bus_class_proxy" => "QueueBus::Rider", "ok" => true}
-      QueueBus::Rider.should_receive(:perform).with(hash).and_raise("rider crash")
-      lambda {
+      expect(QueueBus::Rider).to receive(:perform).with(hash).and_raise("rider crash")
+      expect {
         QueueBus::Worker.perform(JSON.generate(hash))
-      }.should raise_error("rider crash")
+      }.to raise_error("rider crash")
     end
   end
 end
