fdd21a534df646aae78bc1156c1fbb21b3e8746f
kbacha <chewbacha@gmail.com>
2019-08-06 09:24:36 -0700

    Applies autocorrect rules to library

--- lib/sidekiq_bus/middleware/retry.rb
+++ lib/sidekiq_bus/middleware/retry.rb
@@ -1,18 +1,18 @@
+# frozen_string_literal: true
+
 module SidekiqBus
   module Middleware
     module Client
-     
       # ensure sidekiq will retry jobs even when they are enqueued via other adapters
       class Retry
-       def call(worker_class, job, queue, redis_pool)
-         if job['class'] == 'QueueBus::Worker'
-          job['retry'] = true unless job.has_key?('retry')
-          job['backtrace'] = true unless job.has_key?('backtrace')
-         end
-         yield
-       end
+        def call(_worker_class, job, _queue, _redis_pool)
+          if job['class'] == 'QueueBus::Worker'
+            job['retry'] = true unless job.key?('retry')
+            job['backtrace'] = true unless job.key?('backtrace')
+          end
+          yield
+        end
      end
-
    end
  end
 end
